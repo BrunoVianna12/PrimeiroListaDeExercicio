5cc6f6fee17f44de039888c10e1710effece8572
Bruno Vianna <bruno_sv07@hotmail.com>
2019-03-23 13:29:46 -0300

    Add project files.

---
+++ PrimeiroListaDeExercicio/Entities/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroListaDeExercicio.Entities
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ PrimeiroListaDeExercicio/Entities/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroListaDeExercicio.Entities
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string Nome { get; set; }
+        public Decimal Preco { get; set; }
+        public DateTime DataValidade { get; set; }
+        public Categoria Categoria { get; set; }
+
+
+
+    }
+}
