6513759751662d5dfe803175c3d506ec1cd3676e
Bruno Vianna <bruno_sv07@hotmail.com>
2019-03-23 17:10:16 -0300

    Primeira lista de exercicios

--- PrimeiroListaDeExercicio/Program.cs
+++ PrimeiroListaDeExercicio/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PrimeiroListaDeExercicio.Entities;
+using PrimeiroListaDeExercicio.Repositories;
 namespace PrimeiroListaDeExercicio
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Produto produto = new Produto();
+            produto.Categoria = new Categoria();
+
+            Console.WriteLine("\tBem vindo ao Sistema de Cadastro de Produtos");
+            Console.WriteLine("Informe o Id do Produto....: ");
+            produto.IdProduto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o nome do produto..: ");
+            produto.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o preço do produto.: ");
+            produto.Preco = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a Data de Validade.: ");
+            produto.DataValidade = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o ID da categoria");
+
         }
     }
 }
---
+++ PrimeiroListaDeExercicio/Repositories/ProdutoRepositories.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using PrimeiroListaDeExercicio.Entities;
+namespace PrimeiroListaDeExercicio.Repositories
+{
+    public class ProdutoRepositories
+    {
+        public void ExportarParaTxt(Produto produto)
+        {
+            using (StreamWriter writer = new StreamWriter("c:\\temp\\produtos.txt", true))
+            {
+
+                writer.WriteLine($"Produto {produto.Nome}");
+                writer.WriteLine($"\tId do produto.........: {produto.IdProduto}");
+                writer.WriteLine($"\tPreco do Produto......: {produto.Preco}");
+                writer.WriteLine($"\tData de Validade......: {produto.DataValidade}");
+                writer.WriteLine($"\tId Categoria..........: {produto.Categoria.IdCategoria}");
+                writer.WriteLine($"\tDescrição da categoria: {produto.Categoria.Descricao}");
+                
+
+            }
+        }
+    }
+}
